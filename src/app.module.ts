3a5f803d081ae4513ac0f441c43571ecf9abf36e
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2025-09-16 01:53:55 +0200

    add: kafka consumer module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FakeDataModule } from './fake-data/fake-data.module';
+import { LeaderboardsModule } from './leaderboards/leaderboards.module';
 
 @Module({
-  imports: [FakeDataModule],
+  imports: [FakeDataModule, LeaderboardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
