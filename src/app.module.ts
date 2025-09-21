c5ac3a41f9dabf4d38f51ece85fd623cf3d1cb71
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2025-09-21 20:31:39 +0200

    add: postgres client connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,33 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FakeDataModule } from './fake-data/fake-data.module';
 import { StreamProcessingModule } from './stream-processing/stream-processing.module';
-import { RedisModule } from './redis/redis.module';
+import { LeaderboardModule } from './leaderboard/leaderboard.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const POSTGRES_PORT = 5432;
+const POSTGRES_HOST = 'localhost';
+const POSTGRES_USER = 'postgres';
+const POSTGRES_PASSWORD = 'postgres';
+const POSTGRES_DB = 'leaderboard';
+const AUTOLOAD_ENTITIES = true;
+const SYNCHRONIZE = process.env.NODE_ENV !== 'production';
 
 @Module({
-  imports: [FakeDataModule, StreamProcessingModule, RedisModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: POSTGRES_HOST,
+      port: POSTGRES_PORT,
+      username: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+      database: POSTGRES_DB,
+      autoLoadEntities: AUTOLOAD_ENTITIES,
+      synchronize: SYNCHRONIZE,
+    }),
+    FakeDataModule,
+    StreamProcessingModule,
+    LeaderboardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
